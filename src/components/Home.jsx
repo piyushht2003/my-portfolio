7d964f9fca0c357a78ccfc5608b22e4184261eac
Piyush <piyushsinghthakur128@gmail.com>
2025-06-28 01:43:12 +0530

    adjusted opacity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
           id="home"
           className="bg-zinc-950 min-h-screen w-full  px-6 py-16 md:px-10   md:py-16 lg:px-20 lg:py-16 flex flex-col justify-center items-center"
         >
-          <div className="w-full max-w-7xl bg-[url('/images/profile.png')] bg-center bg-cover bg-no-repeat bg-fixed bg-blend-overlay bg-zinc-900/50 rounded-4xl shadow-9xl p-6 md:p-10 lg:p-16 flex flex-col justify-center items-center">
+          <div className="w-full max-w-7xl bg-[url('/images/profile.png')] bg-center bg-contain bg-no-repeat bg-fixed bg-blend-overlay bg-zinc-900/85 rounded-4xl shadow-9xl p-6 md:p-10 lg:p-4 flex flex-col justify-center items-center">
 
           <section className="w-full flex flex-col lg:flex-row justify-center relative items-center gap-10">
             <div className="w-full">
@@ -50,7 +50,7 @@ const Home = () => {
               id="home-right"
               className="w-full h-auto lg:h-screen  flex justify-center items-center"
             >
-              <div className="border px-10 rounded-4xl md:px-16 py-10 md:py-20  text-sky-400 shadow-9xl flex flex-col gap-4 justify-center items-start">
+              <div className="px-10 md:px-16 py-10 md:py-20 flex flex-col gap-4 justify-center items-start">
                 {/* <h1 className="text-3xl md:text-5xl lg:text-6xl text-yellow-400">
                   <i className="ri-edit-circle-fill"></i>developer
                 </h1>
